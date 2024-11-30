2a371ae34cdc9e5c2587e5761fcbd3c58d60c46c
ER <estela2ricardo@gmail.com>
2024-11-30 15:56:27 +0000

    fixed height, improved gitignore, ready for deploy

--- cmsER/settings.py
+++ cmsER/settings.py
@@ -11,6 +11,7 @@
 
 # """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nc9as^#&k72r4#ii5w&sxvlf^mg8my@+1)3qq7ct_(j&-1ci47'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -82,6 +83,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
